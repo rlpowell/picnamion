7787f4504cf0674412cb785827f587902d4147bb
Robin Lee Powell <rlpowell@digitalkingdom.org>
2025-10-24 11:57:49 -0700

    Fixed up make_test_image.sh, added a file type

--- make_test_image.sh
+++ make_test_image.sh
@@ -2,12 +2,26 @@
 
 # This script just makes pictures super tiny so I can upload them
 # without accidentally doing a privacy
-magick "$1" -resize 16x16 "$1.new"
+if [[ $(exiftool -s3 -MIMEType "$1") =~ ^video/ ]]
+then
+  echo "video found, using ffmpeg"
+  ffmpeg -i "$1"  -vf scale=16:16 "$1.new.mp4"
+  mv "$1.new.mp4" "$1.new"
+elif [[ $(exiftool -s3 -MIMEType "$1") =~ ^image/ ]]
+then
+  echo "image found, using magick"
+  magick "$1" -resize 16x16 "$1.new"
+else
+  echo "Neither video nor image; bailing."
+  exit 1
+fi
+
 if [[ -f "$1.new" ]]
 then
-    exiftool -overwrite_original_in_place -tagsFromFile "$1" "$1.new"
-    touch -r "$1" "$1.new"
-    mv "$1.new" "$1"
+  exiftool -all= "$1.new"
+  exiftool -overwrite_original_in_place -tagsFromFile "$1" "$1.new"
+  touch -r "$1" "$1.new"
+  mv "$1.new" "$1"
 else
-    echo "FAILED: $1"
+  echo "FAILED: $1"
 fi
